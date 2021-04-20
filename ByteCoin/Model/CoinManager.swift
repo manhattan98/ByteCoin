e48f5f9fe8d467fd12a9bb94b868d1584104c34f
Eremej <55193087+manhattan98@users.noreply.github.com>
2021-04-20 22:51:25 +0700

    Update CoinManager.swift

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -32,7 +32,7 @@ struct CoinManager {
     }
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/"
-    let apiKey = "9778EF8D-AB3F-403D-82F3-A3B0980353BE"
+    let apiKey = ""
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
